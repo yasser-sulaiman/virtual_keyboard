3c1c11b52933008f933d5acdc66aaa4d83dbf7c0
yasser-sulaiman <yassersulaiman.is@gmail.com>
2021-09-22 14:52:55 +0300

    scaling the frame and adding calculating dist func

--- virtual keyboard.py
+++ virtual keyboard.py
@@ -14,10 +14,12 @@ def getMousPos(event , x, y, flags, param):
     #     print(x,y)
         mouseX, mouseY = x, y
 
+def calculateIntDidtance(pt1, pt2):
+    return int(((pt1[0]-pt2[0])**2 + (pt1[1]-pt2[1])**2)**0.5)
 
-# Creat keys
-w,h = 50, 50
-startX, startY = 50, 200
+# Creating keys
+w,h = 80, 60
+startX, startY = 40, 420
 keys=[]
 letters =list("QWERTYUIOPASDFGHJKLZXCVBNM")
 for i,l in enumerate(letters):
@@ -29,6 +31,7 @@ for i,l in enumerate(letters):
         keys.append(Key(startX + (i-19)*w + i*5, startY + 2*h + 10, w, h, l)) 
 
 keys.append(Key(startX+25, startY+3*h+15, 5*w, h, "Space"))
+keys.append(Key(startX+8*w + 50, startY+2*h+10, w, h, "clr"))
 keys.append(Key(startX+5*w+30, startY+3*h+15, 5*w, h, "<--"))
 
 showKey = Key(300,5,80,50, 'Show')
@@ -38,12 +41,13 @@ textBox = Key(startX, startY-h-5, 10*w+9*5, h,'')
 cap = cv2.VideoCapture(0)
 ptime = 0
 
-tracker = HandTracker()
+# initiating the hand tracker
+tracker = HandTracker(detectionCon=0.8)
 
 # getting frame's height and width
 frameHeight, frameWidth, _ = cap.read()[1].shape
-showKey.x = frameWidth - 85
-exitKey.x = frameWidth - 85
+showKey.x = int(frameWidth*1.5) - 85
+exitKey.x = int(frameWidth*1.5) - 85
 #print(showKey.x)
 
 clickedX, clickedY = 0, 0
@@ -51,7 +55,12 @@ mousX, mousY = 0, 0
 
 show = False
 cv2.namedWindow('video')
+counter = 0
+previousClick = 0
 while True:
+    if counter >0:
+        counter -=1
+        
     signTipX = 0
     signTipY = 0
 
@@ -61,15 +70,19 @@ while True:
     ret, frame = cap.read()
     if not ret:
         break
-
+    frame = cv2.resize(frame,(int(frameWidth*1.5), int(frameHeight*1.5)))
+    #frame = cv2.flip(frame, 1)
     #find hands
     frame = tracker.findHands(frame)
     lmList = tracker.getPostion(frame, draw=False)
     if lmList:
         signTipX, signTipY = lmList[8][1], lmList[8][2]
         thumbTipX, thumbTipY = lmList[4][1], lmList[4][2]
-        #cv2.circle(frame, (signTipX, signTipY), 15, (50,50,50), cv2.FILLED)
-        #cv2.circle(frame, (thumbTipX, thumbTipY), 15, (50,50,50), cv2.FILLED)
+        if calculateIntDidtance((signTipX, signTipY), (thumbTipX, thumbTipY)) <50:
+            centerX = int((signTipX+thumbTipX)/2)
+            centerY = int((signTipY + thumbTipY)/2)
+            cv2.line(frame, (signTipX, signTipY), (thumbTipX, thumbTipY), (0,255,0),2)
+            cv2.circle(frame, (centerX, centerY), 5, (0,255,0), cv2.FILLED)
 
     ctime = time.time()
     fps = int(1/(ctime-ptime))
@@ -88,26 +101,41 @@ while True:
         #break
         exit()
 
+    #checking if sign finger is over a key and if click happens
     alpha = 0.5
     if show:
         textBox.drawKey(frame, (255,255,255), (0,0,0), 0.3)
         for k in keys:
             if k.isOver(mouseX, mouseY) or k.isOver(signTipX, signTipY):
                 alpha = 0.1
+                # writing using mouse right click
+                if k.isOver(clickedX, clickedY):                              
+                    if k.text == '<--':
+                        textBox.text = textBox.text[:-1]
+                    elif k.text == 'clr':
+                        textBox.text = ''
+                    elif len(textBox.text) < 30:
+                        if k.text == 'Space':
+                            textBox.text += " "
+                        else:
+                            textBox.text += k.text
+                # writing using fingers
+                if (k.isOver(thumbTipX, thumbTipY)):
+                    clickTime = time.time()
+                    if clickTime - previousClick > 0.4:                               
+                        if k.text == '<--':
+                            textBox.text = textBox.text[:-1]
+                        elif k.text == 'clr':
+                            textBox.text = ''
+                        elif len(textBox.text) < 30:
+                            if k.text == 'Space':
+                                textBox.text += " "
+                            else:
+                                textBox.text += k.text
+                        previousClick = clickTime
             k.drawKey(frame,(255,255,255), (0,0,0), alpha=alpha)
             alpha = 0.5
-
-            if k.isOver(clickedX, clickedY) or (k.isOver(signTipX, signTipY) and k.isOver(thumbTipX, thumbTipY)):
-                if k.text == '<--':
-                    textBox.text = textBox.text[:-1]
-                elif len(textBox.text) < 30:
-                    if k.text == 'Space':
-                        textBox.text += " "
-                    else:
-                        textBox.text += k.text
-            
-        clickedX, clickedY = 0, 0
-        
+        clickedX, clickedY = 0, 0        
     ptime = ctime
     cv2.imshow('video', frame)
 
